873eaa64a64bfb0d73ccef4e89247aa8b6829693
Soso1010 <99210584+Soso1010@users.noreply.github.com>
2023-01-31 11:04:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,17 @@ function modelLoaded(){
 
 function draw(){
     image(video, 0, 0, 380, 380);
-    if(staus
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: objetos detectados";
+            
+            fill("#FF0000");
+            percent - floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15,  objects[i].y + 15);
+            noFill();
+            stroke("FF0000");
+            rect( objects[i].x,  objects[i].y,  objects[i].,  objects[i].
 }
 function gotResult(error, results){
     if (error){
